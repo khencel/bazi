ab3ecfff7726216f444fc01e8e2ff95d6214e32f
khencel <khenneth.alaiza@gmail.com>
2026-03-26 19:32:52 +0800

    update

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -199,9 +199,17 @@ export default function HomePage(){
                                         <option value="Female">Female</option>
                                     </select>
                                 </div>
+                                <div className="row">
+                                    <div className="col">
+                                        <input type="date" className="form-control" />
+                                    </div>
+                                    <div className="col">
+                                        <input type="time" className="form-control" />
+                                    </div>
+                                </div>
 
                                 {/* Date of Birth */}
-                                <div className={s["destiny-field"]}>
+                                {/* <div className={s["destiny-field"]}>
                                     <label>Date of Birth</label>
                                     <input
                                     className="form-control"
@@ -213,20 +221,20 @@ export default function HomePage(){
                                         pattern="\d{4}-\d{2}-\d{2}"
                                         placeholder="YYYY-MM-DD"
                                     />
-                                </div>
+                                </div> */}
 
                                 {/* Time */}
-                                <div className={s["destiny-field"]}>
-                                <label>Time of Birth</label>
-                                <input
-                                    className="tob"
-                                    type="time"
-                                    name="time"
-                                    value={form.time}
-                                    onChange={handleChange}
-                                    step="60"
-                                />
-                                </div>
+                                {/* <div className={s["destiny-field"]}>
+                                    <label>Time of Birth</label>
+                                    <input
+                                        className="tob"
+                                        type="time"
+                                        name="time"
+                                        value={form.time}
+                                        onChange={handleChange}
+                                        step="60"
+                                    />
+                                </div> */}
                                 
 
                                 {/* Button */}
