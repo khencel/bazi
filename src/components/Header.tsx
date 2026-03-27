f702123e3762dc99aae6f0cb6d4bea74bd6a3dec
khencel <khenneth.alaiza@gmail.com>
2026-03-27 13:45:59 +0800

    update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,35 @@
 "use client";
+
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Cookies from "js-cookie";
+import { alertPopup } from "./Toaster";
 
 export default function Header() {
     const router = useRouter();
     const [menuOpen, setMenuOpen] = useState(false);
+    const [hasToken, setHasToken] = useState(false);
+
+    useEffect(() => {
+        const token = Cookies.get("access"); // palitan mo kung iba name ng cookie mo
+        setHasToken(!!token);
+    }, []);
 
     const goTo = (path: string) => {
         router.push(path);
         setMenuOpen(false);
     };
+
+    const handleLogout = () => {
+        alertPopup({
+            title: "Logout?",
+            text: "You will be logged out!",
+            confirmText: "Yes, logout",
+            onConfirm: async () => {
+                
+            }
+        });
+    }
 
     return (
         <header>
@@ -37,20 +57,40 @@ export default function Header() {
 
                     <div className={`nav-right ${menuOpen ? "open" : ""}`}>
                         <nav className="navlinks" aria-label="Primary">
-                            <a href="#" onClick={() => goTo("/")}>Home</a>
-                            <a href="#how" onClick={() => setMenuOpen(false)}>About Us</a>
-                            <a href="#pricing" onClick={() => setMenuOpen(false)}>Data Privacy</a>
-                            <a href="#faq" onClick={() => setMenuOpen(false)}>Copyright Notice</a>
-                        </nav>
-
+                        {
+                            !hasToken && (
+                                <>
+                                    <a href="#" onClick={() => goTo("/")}>Home</a>
+                                    <a href="#how" onClick={() => setMenuOpen(false)}>About Us</a>
+                                    <a href="#pricing" onClick={() => setMenuOpen(false)}>Data Privacy</a>
+                                    <a href="#faq" onClick={() => setMenuOpen(false)}>Copyright Notice</a>
+                                </>
+                            )
+                        }
+                        
                         <div className="navcta">
-                            <button className="btn" onClick={() => goTo("/login")}>
-                                Sign In
-                            </button>
-                            <button className="btn primary" onClick={() => goTo("/register")}>
-                                Sign Up
-                            </button>
+                            {hasToken ? (
+                                <>
+                                    <a href="#" onClick={() => goTo("/")}>Khenneth Alaiza</a>
+                                    <button
+                                        className="btn primary"
+                                        onClick={handleLogout}
+                                    >
+                                        Sign Out
+                                    </button>
+                                </>
+                            ) : (
+                                <>
+                                    <button className="btn" onClick={() => goTo("/login")}>
+                                        Sign In
+                                    </button>
+                                    <button className="btn primary" onClick={() => goTo("/register")}>
+                                        Sign Up
+                                    </button>
+                                </>
+                            )}
                         </div>
+                        </nav>
                     </div>
                 </div>
             </div>
--- src/components/Toaster.tsx
+++ src/components/Toaster.tsx
@@ -1,5 +1,70 @@
 import Swal from "sweetalert2";
 import s from "../../public/css/login.module.css";
+import h from "../../public/css/home.module.css"
+
+type AlertPopupProps = {
+  title?: string;
+  text?: string;
+  icon?: "warning" | "error" | "success" | "info" | "question";
+  confirmText?: string;
+  cancelText?: string;
+  successTitle?: string;
+  successText?: string;
+  showSuccess?: boolean;
+  onConfirm?: (() => Promise<void> | void) | null;
+};
+
+export const alertPopup = async ({
+  title = "Are you sure?",
+  text = "This action cannot be undone.",
+  icon = "warning",
+  confirmText = "Yes",
+  cancelText = "Cancel",
+  successTitle = "Success!",
+  successText = "",
+  showSuccess = true,
+  onConfirm = null,
+}: AlertPopupProps) => {
+  const result = await Swal.fire({
+    title,
+    text,
+    icon,
+    background: "#1e1e1e",
+    color: "#fff",
+    showCancelButton: true,
+    confirmButtonText: confirmText,
+    cancelButtonText: cancelText,
+    buttonsStyling: false, // ⚠️ required
+
+    customClass: {
+      actions: h["swal-actions"],
+      confirmButton: h["confirm-btn"],
+      cancelButton: h["cancel-btn"],
+    },
+  });
+
+  if (result.isConfirmed) {
+    if (onConfirm) {
+      await onConfirm();
+    }
+
+    if (showSuccess) {
+      await Swal.fire({
+        title: successTitle,
+        text: successText,
+        icon: "success",
+        background: "#1e1e1e",
+        color: "#fff",
+        timer: 1500,
+        showConfirmButton: false,
+      });
+    }
+
+    return true;
+  }
+
+  return false;
+};
 
 export const showToast = (
   message: string,
